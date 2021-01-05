f9329f36a46818bdf6426125e8de2ca2fce4c6f8
Haneul Jeong <haneulj0510@gmail.com>
2021-01-05 19:51:10 +0900

    1/5 실습3

---
+++ conditionLab4.py
@@ -0,0 +1,7 @@
+import random
+grade = random.randint(1, 6)
+
+if grade == 1 or grade == 2 or grade == 3 :
+    print("grade",grade, "학년은 저학년 입니다.")
+else :
+    print("grade",grade, "학년은 고학년 입니다.")
